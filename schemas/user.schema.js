ad6ca73e7a840a6fd6fa1c4f3dfed740ade894a7
musiur <musiur.opu@gmail.com>
2023-08-02 13:32:55 +0600

    controller-dev:: auth controller: changes based on schema

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,24 +1,17 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  contactDetails: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    phone: { type: String, required: true },
-  },
   password: { type: String, required: true },
-  servicesList: [
-    {
-      serviceId: { type: String, required: false },
-      name: { type: String, required: false },
-      titleImage: { type: String, required: false },
-      streetAddress: { type: String, required: false },
-      serviceRefId: { type: String, required: false },
-    },
-  ],
-  verifiedStatus: { type: Boolean, required: false },
+  phone: { type: String, required: true },
+  registerInfo: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+  },
+  verification: {type: Boolean, required: true, default: false},
+  email: { type: String, required: true, unique: true },
+  servicesList: {type: Array, required: false, default: []},
   createdAt: { type: Date, default: Date.now },
 })
 
 module.exports = userSchema
+
